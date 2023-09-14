425bfb5525299b641d2e11645a79a00fbdfcc1be
André Biasuz <58049577+Andrebiasuz@users.noreply.github.com>
2023-09-14 12:06:10 +0000

    First day of Code100

---
+++ 100d/100d_temp_education_files/d1/d1_proj_bandgenerator.py
@@ -0,0 +1,9 @@
+pet_name = " "
+city = " "
+
+print("Hello to band name generator!")
+
+pet_name = input("What is your pet's name? ")
+city = input("What is the city you grew up in? ")
+
+print("Name of the band is: " + city + " " + pet_name)

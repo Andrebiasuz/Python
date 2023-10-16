1f937ca8a5f889b20d4c8b198743818ece2f8222
André Biasuz <58049577+Andrebiasuz@users.noreply.github.com>
2023-10-16 16:35:18 +0000

    Basic test passed, improving by adding failed large shift test

--- 100d/100d_temp_education_files/d8/ceasar_cypher_functions.py
+++ 100d/100d_temp_education_files/d8/ceasar_cypher_functions.py
@@ -29,34 +29,34 @@ def ceasar_cypher_encode(string_to_encode, cypher_shift):
             encoded_index = position_in_alfabet_string + cypher_shift
 
             if (position_in_alfabet_string + cypher_shift) >= len(alfabet_lower):
-                encoded_index = encoded_index - len(alfabet_lower)
+                encoded_index = encoded_index - len(alfabet_lower) % 26
 
             if position_in_alfabet_string == -1:
                 encoded = encoded + " "
 
             else:
-                
+
                 if string_to_encode[index].isupper():
                     encoded = encoded + alfabet_upper[encoded_index]
                 else:    
-                    encoded = encoded + alfabet_lower[encoded_index]
-                
+                    encoded = encoded + alfabet_lower[encoded_index]            
 
     return encoded
 
-# TODO: Add upper and lower to alfabet. 
 def ceasar_cypher_decode(string_to_decode, cypher_shift):
     """Function receives an encoded string acording to Ceasar Cypher method ,
     and a integer as the cypher's shift. Returns the decoded string. Needs the string library,
     """
 
-    alfabet = string.ascii_lowercase
+    alfabet_lower = string.ascii_lowercase
+    alfabet_upper = string.ascii_uppercase
+
     string_to_decode_lower = string_to_decode.lower()
 
     decoded = ""
 
     for index, value in enumerate(string_to_decode_lower):
-        position_in_alfabet_string = alfabet.find(string_to_decode_lower[index])
+        position_in_alfabet_string = alfabet_lower.find(string_to_decode_lower[index])
 
         if string_to_decode[index] in string.punctuation:
 
@@ -67,7 +67,7 @@ def ceasar_cypher_decode(string_to_decode, cypher_shift):
             decoded_index = position_in_alfabet_string - cypher_shift
 
             if (position_in_alfabet_string + cypher_shift) < 0:
-                decoded_index = decoded_index + len(alfabet)
+                decoded_index = decoded_index + len(alfabet_lower)%26
 
             if decoded_index == 26:
                 decoded_index = 0
@@ -75,6 +75,10 @@ def ceasar_cypher_decode(string_to_decode, cypher_shift):
             if position_in_alfabet_string == -1:
                 decoded = decoded + " "
             else:
-                decoded = decoded + alfabet[decoded_index]
+
+                if string_to_decode[index].isupper():
+                    decoded = decoded + alfabet_upper[decoded_index]
+                else:    
+                    decoded = decoded + alfabet_lower[decoded_index]                
 
     return decoded
--- 100d/100d_temp_education_files/d8/ceasar_cypher_test.py
+++ 100d/100d_temp_education_files/d8/ceasar_cypher_test.py
@@ -5,13 +5,23 @@ import ceasar_cypher_functions
 
 class CaesarCipherTestCase(unittest.TestCase):
     def test_encrypt(self):
-        plaintext = "Hello, World!"
+        plaintext = "HelLo, WoRld!"
         encrypted = ceasar_cypher_functions.ceasar_cypher_encode(plaintext, 3)
-        self.assertEqual(encrypted, "Khoor, Zruog!")
+        self.assertEqual(encrypted, "KhoOr, ZrUog!")
 
     def test_decrypt(self):
         ciphertext = "Khoor, Zruog!"
         decrypted = ceasar_cypher_functions.ceasar_cypher_decode(ciphertext, 3)
+        self.assertEqual(decrypted, "Hello, World!")
+
+    def test_encrypt_large_shift(self):
+        plaintext = "HelLo, WoRld!"
+        encrypted = ceasar_cypher_functions.ceasar_cypher_encode(plaintext, 355)
+        self.assertEqual(encrypted, "Yvccf, Nficu!")
+
+    def test_decrypt_large_shift(self):
+        ciphertext = "Yvccf, Nficu!"
+        decrypted = ceasar_cypher_functions.ceasar_cypher_decode(ciphertext, 355)
         self.assertEqual(decrypted, "Hello, World!")
 
 if __name__ == "__main__":

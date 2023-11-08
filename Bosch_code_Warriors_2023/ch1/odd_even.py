7319509b5dd8bc361989ff6d0bb06702a37e8d7b
André Biasuz <58049577+Andrebiasuz@users.noreply.github.com>
2023-11-08 16:15:26 +0000

    Codewarriors challenge from Bosch day1

---
+++ Bosch_code_Warriors_2023/ch1/odd_even.py
@@ -0,0 +1,12 @@
+def even_or_odd(number):
+    if not number % 2 :
+        return "Even"
+    return "Odd"
+
+
+import datetime
+start_time = datetime.datetime.now()
+for i in range(0,1000000):
+    a = (even_or_odd(2))
+end_time = datetime.datetime.now()
+print(end_time - start_time)
